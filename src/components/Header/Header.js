cfb1c8bc6e1a1929e414926d018962079288db7e
MrMetrail <zenroeski@aol.com>
2019-07-26 16:05:43 -0600

    components without style

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => (
+    <header className="header">
+        <h1>Clicky Game</h1>
+        <h2>Click on each image once, and ONLY once</h2>
+    </header>
+);
+
+export default Header;
