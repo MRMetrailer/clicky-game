35bcafd7e596d210adb59b28cb49dbd8a96ad686
MrMetrail <zenroeski@aol.com>
2019-07-30 20:50:43 -0600

    Updates

---
+++ precache-manifest.eab4dce6397463faea872b60bf6067ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26dd70ecd91a9a1b7cdb5a7f04370a48",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1257224b33d62ea33766",
+    "url": "/clicky-game/static/css/main.9b6639d2.chunk.css"
+  },
+  {
+    "revision": "21475265196432d20b58",
+    "url": "/clicky-game/static/js/2.8cac1a3c.chunk.js"
+  },
+  {
+    "revision": "1257224b33d62ea33766",
+    "url": "/clicky-game/static/js/main.6641d39e.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "37981b3ea48737fe8a73f7ab1b36fac4",
+    "url": "/clicky-game/static/media/birds.37981b3e.jpg"
+  }
+]);
